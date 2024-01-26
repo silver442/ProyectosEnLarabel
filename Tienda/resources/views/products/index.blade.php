52ef977a6a30bdd22381255f8f79ca94cf3afedd
Silvestre Hernandez <silvestre.hdz.h@gmail.com>
2024-01-26 14:41:50 -0600

    creacion de ruta Listado de Productos

---
+++ Tienda/resources/views/products/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Listado de productos
+                        <a href="" class="btn btn-success btn-sm float-end">Nuevo productos</a>
+                    </div>
+                    <div class="card-body">
+                        ...
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
